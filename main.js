52a3e2ecb0db3366e5f5b91f3c7470a2cabe7685
Ray Minge <oranj@users.noreply.github.com>
2014-11-20 23:59:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 var X = require("X"), 
-    creepName;
+    creepName, 
+    xCreep;
 
 for (creepName in Game.creeps) {
     if (Game.creeps.hasOwnProperty(creepName)) {
-        X(Game.creeps[creepName]).harvestEnergy();
+        xCreep = X(Game.creeps[creepName]);
+        switch (xCreep.getRole()) {
+            case 'guard':
+                xCreep.defend();
+                break;
+            case 'builder':
+                xCreep.construct();
+                break;
+            case 'harvester':
+                xCreep.harvestEnergy();
+                break;
+            default: 
+                var build = creepName.match(/^([a-zA-Z]+)[0-9]+/)[1];
+                switch (build) {
+                    case 'Worker':
+                        xCreep.setRole('harvester');
+                        break;
+                    case 'Guard':
+                        xCreep.setRole('guard');
+                        break;
+                    case 'Builder':
+                        xCreep.setRole('builder');
+                        break;
+                }
+        }
     }
 }
